ce2e7e60b136a71c0413b4730d760fb8ae08c163
njc73 <njc2536+github@gmail.com>
2025-05-04 12:15:38 -0400

    wtf 404

---
+++ nav.js
@@ -0,0 +1,6 @@
+<a href="/index.html">Home</a>
+<a href="/osint.html">OSINT Writeups</a>
+<a href="/puzzles.html">Puzzles</a>
+<a href="/truisms.html"><img src = "/img/click.gif" width = 60%></a>
+<a href="/music.html">μzik</a>
+<a href="/rick.html">くコ:彡</a>
---
+++ navbar.php
@@ -0,0 +1,14 @@
+<nav>
+	<div>
+	<!-- Optional image in sidebar, will be resized if it's too big -->
+	<!-- <img src="" alt=""> -->
+	<a href="/index.html">Home</a>
+	<a href="/osint.html">OSINT Writeups</a>
+	<a href="/puzzles.html">Puzzles</a>
+	<a href="/truisms.html"><img src = "/click.gif" width = 60%></a>
+	<!-- Up to 8 links fit comfortably on my small laptop screen with the image used in the preview;
+			Without an image, up to 14 links fit -->
+    <a href="/music.html">μzik</a>
+	<a href="/rick.html">くコ:彡</a>
+    </div>
+</nav>
